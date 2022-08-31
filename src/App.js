c965c0ad511c35c2f7866521beffbd7f87551b62
ritiksharmarj <bloggeritik@gmail.com>
2022-09-01 00:06:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className='w-full bg-primary overflow-hidden'>
+    <h1>Hola Ritik</h1>
+  </div>
+);
 
 export default App;

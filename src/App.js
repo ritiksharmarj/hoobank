5225560e94acbbf2c986e365543658cf1fb6fae2
ritiksharmarj <bloggeritik@gmail.com>
2022-09-02 02:49:01 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import style from './style';
 
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from './components';
+
 const App = () => (
   <div className='w-full bg-primary overflow-hidden'>
     {/* Navbar */}
     <div className={`${style.paddingX} ${style.flexCenter}`}>
-      <div className={`${style.boxWidth}`}>Navbar</div>
+      <div className={`${style.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
 
     {/* Hero section */}
+    <div className={`${style.flexStart} bg-primary`}>
+      <div className={`${style.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`${style.flexStart} ${style.paddingX} bg-primary`}>
+      <div className={`${style.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
   </div>
 );
 
